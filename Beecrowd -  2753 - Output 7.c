3935603962a142e342f25e85a0550abeca05bdd9
Emon Sharkar <128619771+emonsharkar@users.noreply.github.com>
2024-09-01 12:58:37 +0600

    Create Beecrowd -  2753 - Output 7.c

---
+++ Beecrowd -  2753 - Output 7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+ int
+a=97,b=98,c=99,d=100,e=101,f=102,g=103,h=104,i=105,j=106,k=107,l=108,m=109,n=110,o=111,p=112,q=113,r=114
+,s=115,t=116,u=117,v=118,w=119,x=120,y=121,z=122;
+ printf("%d e a\n",a);
+ printf("%d e b\n",b);
+ printf("%d e c\n",c);
+ printf("%d e d\n",d);
+ printf("%d e e\n",e);
+ printf("%d e f\n",f);
+ printf("%d e g\n",g);
+ printf("%d e h\n",h);
+ printf("%d e i\n",i);
+ printf("%d e j\n",j);
+ printf("%d e k\n",k);
+ printf("%d e l\n",l);
+ printf("%d e m\n",m);
+ printf("%d e n\n",n);
+ printf("%d e o\n",o);
+ printf("%d e p\n",p);
+ printf("%d e q\n",q);
+ printf("%d e r\n",r);
+ printf("%d e s\n",s);
+ printf("%d e t\n",t);
+ printf("%d e u\n",u);
+ printf("%d e v\n",v);
+ printf("%d e w\n",w);
+ printf("%d e x\n",x);
+ printf("%d e y\n",y);
+ printf("%d e z\n",z);
+ return 0;
+}
