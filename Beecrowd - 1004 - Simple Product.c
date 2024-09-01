8afc2cc56d87260e2b3cbe9fdb6a364783c39ad7
Emon Sharkar <128619771+emonsharkar@users.noreply.github.com>
2024-09-01 12:41:03 +0600

    Create Beecrowd - 1004 - Simple Product.c

---
+++ Beecrowd - 1004 - Simple Product.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int int1,int2,PROD;
+    scanf("%d", &int1);
+    scanf("%d", &int2);
+    PROD = int1*int2;
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
